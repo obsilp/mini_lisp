5df1f7177ea15ba93fb82999a22521046cdc37de
Tim Oster <me@timoster.me>
2019-10-29 13:22:36 +0100

    Improve lexer readability by introducing a custom stream object

--- lexer.go
+++ lexer.go
@@ -45,7 +45,7 @@ type Token struct {
 	Str  string
 }
 
-var consumers = []func([]rune, *int) TokenType{
+var consumers = []func(*stream) TokenType{
 	consumeWhitespace,
 	consumeNewline,
 	consumeComment,
@@ -55,111 +55,141 @@ var consumers = []func([]rune, *int) TokenType{
 	consumeSymbol,
 }
 
+type stream struct {
+	buffer   []rune
+	startPos int
+	pos      int
+}
+
+func (s *stream) Current() rune {
+	if !s.Done() {
+		return s.buffer[s.pos]
+	}
+	return rune(0)
+}
+
+func (s *stream) Read() rune {
+	c := s.Current()
+	s.pos++
+	return c
+}
+
+func (s *stream) Reset() {
+	s.pos = s.startPos
+}
+
+func (s *stream) HasAdvanced() bool {
+	return s.pos > s.startPos
+}
+
+func (s *stream) ReadToken() string {
+	if !s.HasAdvanced() {
+		panic("pointer was not advanced before reading token")
+	}
+	token := s.buffer[s.startPos:s.pos]
+	s.startPos = s.pos
+	return string(token)
+}
+
+func (s *stream) Done() bool {
+	return s.pos >= len(s.buffer)
+}
+
 func Tokenize(input string) []Token {
 	var tokens []Token
 
-	pos := 0
-	stream := []rune(input)
+	s := &stream{buffer: []rune(input)}
 
 	currentLine := 1
 	lineStartPos := 0
 
-	for pos < len(input) {
-		start := pos
-		t := consumeNext(stream, &pos)
+	for !s.Done() {
+		t := consumeNext(s)
 
 		if t == TokenTypeIgnore {
 			continue
 		}
 		if t == TokenTypeInvalid {
-			panic(fmt.Sprintf("invalid char '%c' at line %d:%d", stream[pos], currentLine, pos-lineStartPos+1))
+			panic(fmt.Sprintf("invalid char '%c' at line %d:%d", s.Current(), currentLine, s.pos-lineStartPos+1))
 		}
 
-		str := input[start:pos]
-		token := Token{Type: t, Str: str}
+		token := Token{Type: t, Str: s.ReadToken()}
 		tokens = append(tokens, token)
 
 		if t == TokenTypeNewline {
 			currentLine++
-			lineStartPos = pos + 1
+			lineStartPos = s.pos + 1
 		}
 	}
 
 	return tokens
 }
 
-func consumeNext(stream []rune, pos *int) TokenType {
-	start := *pos
-
+func consumeNext(s *stream) TokenType {
 	for _, c := range consumers {
-		*pos = start
-		t := c(stream, pos)
+		s.Reset()
+		t := c(s)
 		if t != TokenTypeIgnore {
 			return t
 		}
 	}
 
 	// ignore any other control characters
-	if unicode.IsControl(stream[*pos]) {
+	if unicode.IsControl(s.Read()) {
 		return TokenTypeIgnore
 	}
 
 	return TokenTypeInvalid
 }
 
-func consumeWhitespace(stream []rune, pos *int) TokenType {
-	startPos := *pos
-	for *pos < len(stream) && stream[*pos] == ' ' {
-		*pos++
+func consumeWhitespace(s *stream) TokenType {
+	for s.Current() == ' ' {
+		s.Read()
 	}
-	if *pos != startPos {
+	if s.HasAdvanced() {
 		return TokenTypeWhitespace
 	}
 	return TokenTypeIgnore
 }
 
-func consumeNewline(stream []rune, pos *int) TokenType {
-	if stream[*pos] == '\n' {
-		*pos++
+func consumeNewline(s *stream) TokenType {
+	if s.Read() == '\n' {
 		return TokenTypeNewline
 	}
 	return TokenTypeIgnore
 }
 
-func consumeComment(stream []rune, pos *int) TokenType {
-	if stream[*pos] != '#' {
+func consumeComment(s *stream) TokenType {
+	if s.Read() != '#' {
 		return TokenTypeIgnore
 	}
-	for *pos < len(stream) && stream[*pos] != '\n' {
-		*pos++
+	for s.Current() != 0 && s.Current() != '\n' {
+		s.Read()
 	}
 	return TokenTypeComment
 }
 
-func consumeExpressionOpen(stream []rune, pos *int) TokenType {
-	if stream[*pos] != '(' {
-		return TokenTypeIgnore
+func consumeExpressionOpen(s *stream) TokenType {
+	if s.Read() == '(' {
+		return TokenTypeExpOpen
 	}
-	*pos++
-	return TokenTypeExpOpen
+	return TokenTypeIgnore
 }
 
-func consumeExpressionClose(stream []rune, pos *int) TokenType {
-	if stream[*pos] != ')' {
-		return TokenTypeIgnore
+func consumeExpressionClose(s *stream) TokenType {
+	if s.Read() == ')' {
+		return TokenTypeExpClose
 	}
-	*pos++
-	return TokenTypeExpClose
+	return TokenTypeIgnore
 }
 
-func consumeInt(stream []rune, pos *int) TokenType {
-	c := stream[*pos]
+func consumeInt(s *stream) TokenType {
+	c := s.Read()
 
 	if !unicode.IsDigit(c) {
 		if c == '+' || c == '-' {
 			// do not consume the token if the sign is not followed by a number
-			*pos++
-			if *pos >= len(stream) || !unicode.IsDigit(stream[*pos]) {
+			if !unicode.IsDigit(s.Read()) {
 				return TokenTypeIgnore
 			}
 		} else {
@@ -167,19 +197,18 @@ func consumeInt(stream []rune, pos *int) TokenType {
 		}
 	}
 
-	for *pos < len(stream) && unicode.IsDigit(stream[*pos]) {
-		*pos++
+	for unicode.IsDigit(s.Current()) {
+		s.Read()
 	}
 	return TokenTypeInt
 }
 
-func consumeSymbol(stream []rune, pos *int) TokenType {
-	const invalid = "()# "
-	startPos := *pos
-	for *pos < len(stream) && !strings.ContainsAny(string(stream[*pos]), invalid) {
-		*pos++
+func consumeSymbol(s *stream) TokenType {
+	const invalid = " ()#"
+	for s.Current() != 0 && !strings.ContainsAny(string(s.Current()), invalid) {
+		s.Read()
 	}
-	if *pos != startPos {
+	if s.HasAdvanced() {
 		return TokenTypeSymbol
 	}
 	return TokenTypeIgnore
